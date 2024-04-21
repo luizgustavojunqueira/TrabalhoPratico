0d5f913454c020234cf51899928a0cf3d2456207
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2024-04-21 13:47:32 -0400

    Implementar função NextToken
    Identifica todos os tokens propostos para a linguagem minijava
    
    co-authored-by: @camilacaceres1d

--- scanner.cpp
+++ scanner.cpp
@@ -1,4 +1,11 @@
 #include "scanner.h"
+#include <vector>
+#include <algorithm>
+
+vector<string> reserved_words = {"boolean", "class", "else", "extends", "false", "if",
+                                 "int", "length", "main", "new", "public", "return",
+                                 "static", "String", "System.out.println", "this", "true",
+                                 "void", "while"};
 
 // Construtor que recebe uma string com o nome do arquivo
 // de entrada e preenche input com seu conteúdo.
@@ -27,7 +34,7 @@ Scanner::Scanner(string input)
     // A próxima linha deve ser comentada posteriormente.
     // Ela é utilizada apenas para verificar se o
     // preenchimento de input foi feito corretamente.
-    cout << this->input;
+    // cout << this->input;
 }
 
 int Scanner::getLine()
@@ -39,11 +46,283 @@ int Scanner::getLine()
 Token *Scanner::nextToken()
 {
     Token *tok;
-    string lexeme;
+    string lexeme = "";
 
-    // TODO
+    int state = 0;
 
-    return tok;
+    while (true)
+    {
+        switch (state)
+        {
+        case 0:
+            if(input[pos] == '\n'){
+                line++;
+            }
+            if (input[pos] == '\0')
+            {
+                tok = new Token(END_OF_FILE);
+                return tok;
+            }
+            if (isalpha(input[pos]))
+            {
+                lexeme += input[pos];
+                state = 26;
+            }
+            else if (isdigit(input[pos]))
+            {
+                state = 1;
+            }
+            else if (input[pos] == '&')
+            {
+                state = 3;
+            }
+            else if (input[pos] == '=')
+            {
+                state = 5;
+            }
+            else if (input[pos] == '<')
+            {
+                state = 8;
+            }
+            else if (input[pos] == '>')
+            {
+                state = 9;
+            }
+            else if (input[pos] == '+')
+            {
+                state = 10;
+            }
+            else if (input[pos] == '-')
+            {
+                state = 11;
+            }
+            else if (input[pos] == '*')
+            {
+                state = 12;
+            }
+            else if (input[pos] == '/')
+            {
+                state = 13;
+            }
+            else if (input[pos] == '!')
+            {
+                state = 14;
+            }
+            else if (input[pos] == '(')
+            {
+                state = 17;
+            }
+            else if (input[pos] == ')')
+            {
+                state = 18;
+            }
+            else if (input[pos] == '[')
+            {
+                state = 19;
+            }
+            else if (input[pos] == ']')
+            {
+                state = 20;
+            }
+            else if (input[pos] == '{')
+            {
+                state = 21;
+            }
+            else if (input[pos] == '}')
+            {
+                state = 22;
+            }
+            else if (input[pos] == ';')
+            {
+                state = 23;
+            }
+            else if (input[pos] == '.')
+            {
+                state = 24;
+            }
+            else if (input[pos] == ',')
+            {
+                state = 25;
+            }
+
+            pos++;
+            break;
+
+        case 1:
+            if (!isdigit(input[pos]))
+            {
+                state = 2;
+            }
+            pos++;
+            break;
+
+        case 2:
+            tok = new Token(NUMBER, INTEGER_LITERAL);
+            pos--;
+            return tok;
+        case 3:
+            if (input[pos] == '&')
+            {
+                state = 4;
+            }
+            else
+            {
+                lexicalError("erro, esperava um operador &");
+            }
+            pos++;
+            break;
+        case 4:
+            tok = new Token(RELOP, AND);
+            return tok;
+        case 5:
+            if (input[pos] == '=')
+            {
+                state = 6;
+            }
+            else
+            {
+                state = 7;
+            }
+            pos++;
+            break;
+        case 6:
+            tok = new Token(RELOP, EQUAL);
+            return tok;
+        case 7:
+            tok = new Token(RELOP, ATRIBB);
+            return tok;
+        case 8:
+            tok = new Token(RELOP, LESSTHAN);
+            return tok;
+        case 9:
+            tok = new Token(RELOP, GREATERTHAN);
+            return tok;
+        case 10:
+            tok = new Token(RELOP, PLUS);
+            return tok;
+        case 11:
+            tok = new Token(RELOP, MINUS);
+            return tok;
+        case 12:
+            tok = new Token(RELOP, MULT);
+            return tok;
+        case 13:
+            if (input[pos] == '/' || input[pos] == '*')
+            {
+                state = 28;
+            }
+            else
+            {
+                tok = new Token(RELOP, DIV);
+                return tok;
+            }
+            break;
+        case 14:
+            if (input[pos] == '=')
+            {
+                state = 15;
+            }
+            else
+            {
+                state = 16;
+            }
+            pos++;
+            break;
+        case 15:
+            tok = new Token(RELOP, NOTEQUAL);
+            return tok;
+        case 16:
+            tok = new Token(RELOP, NOT);
+            return tok;
+        case 17:
+            tok = new Token(SEP, LEFTPAREN);
+            return tok;
+        case 18:
+            tok = new Token(SEP, RIGHTPAREN);
+            return tok;
+        case 19:
+            tok = new Token(SEP, LEFTBRACKET);
+            return tok;
+        case 20:
+            tok = new Token(SEP, RIGTHBRACKET);
+            return tok;
+        case 21:
+            tok = new Token(SEP, LEFTCURLYBRACKET);
+            return tok;
+        case 22:
+            tok = new Token(SEP, RIGHTCURLYBRACKET);
+            return tok;
+        case 23:
+            tok = new Token(SEP, SEMICOLON);
+            return tok;
+        case 24:
+            tok = new Token(SEP, DOT);
+            return tok;
+        case 25:
+            tok = new Token(SEP, COMMA);
+            return tok;
+        case 26:
+            if (!isalnum(input[pos]) && input[pos] != '_' && input[pos] != '.')
+            {
+                state = 27;
+            }
+            else
+            {
+                lexeme += input[pos];
+            }
+            pos++;
+            break;
+        case 27:
+            if (find(reserved_words.begin(), reserved_words.end(), lexeme) != reserved_words.end())
+            {
+                tok = new Token(RESERVED_WORD, lexeme);
+            }
+            else
+            {
+                tok = new Token(ID, lexeme);
+            }
+
+            pos--;
+            return tok;
+        case 28:
+            if (input[pos] == '/')
+            {
+                state = 29;
+            }
+            else if (input[pos] == '*')
+            {
+                state = 30;
+            }
+            pos++;
+            break;
+        case 29:
+            if (input[pos] == '\n')
+            {
+                state = 0;
+                line++;
+            }
+            pos++;
+            break;
+        case 30:
+            if (input[pos] == '*')
+            {
+                state = 31;
+            }
+            pos++;
+            break;
+        case 31:
+            if (input[pos] == '/')
+            {
+                state = 0;
+            }
+            else
+            {
+                state = 30;
+            }
+            pos++;
+            break;
+        }
+    }
 }
 
 void Scanner::lexicalError(string msg)
--- token.h
+++ token.h
@@ -1,40 +1,86 @@
 #include <iostream>
-#include <ctype.h>//Funções de caracteres
+#include <ctype.h> //Funções de caracteres
 #include <string>
 
 using namespace std;
 
-enum Names 
+enum Names
 {
-    UNDEF,
-    //Continuar
-    //Nomes e atributos dos tokens da linguagem
-    END_OF_FILE
+    UNDEF,             // 0
+    ID,                // 1
+    NUMBER,            // 2
+    INTEGER_LITERAL,   // 3
+    RELOP,             // 4
+    AND,               // 5
+    GREATERTHAN,       // 6
+    LESSTHAN,          // 7
+    PLUS,              // 8
+    MINUS,             // 9
+    MULT,              // 10
+    DIV,               // 11
+    EQUAL,             // 12
+    ATRIBB,            // 13
+    NOTEQUAL,          // 14
+    NOT,               // 15
+    SEP,               // 16
+    LEFTPAREN,         // 17
+    RIGHTPAREN,        // 18
+    LEFTBRACKET,       // 19
+    RIGTHBRACKET,      // 20
+    LEFTCURLYBRACKET,  // 21
+    RIGHTCURLYBRACKET, // 22
+    SEMICOLON,         // 23
+    DOT,               // 24
+    COMMA,             // 25
+    RESERVED_WORD,     // 26
+    BOOLEAN,           // 27
+    CLASS,             // 28
+    ELSE,              // 29
+    EXTENDS,           // 30
+    FALSE,             // 31
+    IF,                // 32
+    INT,               // 33
+    LENGTH,            // 34
+    MAIN,              // 35
+    NEW,               // 36
+    PUBLIC,            // 37
+    RETURN,            // 38
+    STATIC,            // 39
+    STRING,            // 40
+    SOUT,              // 41 System.out.println
+    THIS,              // 42
+    TRUE,              // 43
+    VOID,              // 44
+    WHILE,             // 45
+    COMMENT,           // 46
+    LINE_COMMENT,      // 47
+    BLOCK_COMMENT,     // 48
+    END_OF_FILE        // 49
 };
 
-class Token 
+class Token
 {
-    public: 
-        int name;
-        int attribute;
-        string lexeme;
-    
-        Token(int name)
-        {
-            this->name = name;
-            attribute = UNDEF;
-        }
+public:
+    int name;
+    int attribute;
+    string lexeme;
 
-        Token(int name, string l)
-        {
-            this->name = name;
-            attribute = UNDEF;
-            lexeme = l;
-        }
-        
-        Token(int name, int attr)
-        {
-            this->name = name;
-            attribute = attr;
-        }
+    Token(int name)
+    {
+        this->name = name;
+        attribute = UNDEF;
+    }
+
+    Token(int name, string l)
+    {
+        this->name = name;
+        attribute = UNDEF;
+        lexeme = l;
+    }
+
+    Token(int name, int attr)
+    {
+        this->name = name;
+        attribute = attr;
+    }
 };
